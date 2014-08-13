826159d7310aa18d7b9ce2cbdef7caac6c6ec73d
Andrew Warren-Love <andrew@awarrenlove.com>
2014-08-12 20:21:39 -0400

    Added greasemonkey user scripts

---
+++ greasemonkey/ebsrestyle3.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name EBSrestyle3
+// @namespace http://getsetbro.com/ebsbookmarklet/
+// @description Sunday and Saturday columns are hidden, widths of the hours inputs are dropped down, widths of everything else is bumped up and optimized for a 1920 monitor.
+// @include http://stlcebs.perficient.com:8030/OA_HTML/OA.jsp?_rc=HXCTIMECARDACTIVITIESPAGE*
+// ==/UserScript==
+var css = "#Hxctimecard table{border-collapse:collapse;width:auto;border:0 none}#Hxctimecard [src$='t.gif']{display:none}#Hxctimecard td{vertical-align:top}#Hxctimecard input,#Hxctimecard textarea{border:1px solid #ddd !important;min-height:22px}#Hxctimecard input,#Hxctimecard textarea[cols='25']{background-image:none !important;width:7.5vw;display:block}#Hxctimecard select{border:1px solid #ddd !important;min-width:220px}#Hxctimecard .x1x,#Hxctimecard .x2a,#Hxctimecard .x24{font-weight:normal;background-color:#eee}#Hxctimecard input ~ a{text-decoration:none;display:block;text-align:right;padding:5px 0 0 0}#Hxctimecard #tipIcon img,#Hxctimecard a img{display:none}#Hxctimecard button,#Hxctimecard #tipIcon:after,#Hxctimecard input ~ a:after{background-color:#69c;color:#fff;padding:0 5px;border-radius:8px}#Hxctimecard button{border:0 none;background-image:none !important;font-weight:normal}#Hxctimecard input ~ a:after{content:'Find';display:inline-block}#Hxctimecard #tipIcon:after{content:'Info';display:inline-block}#Hxctimecard a:hover:after,#Hxctimecard button:hover{background-color:#369}#Hxctimecard [title~=Project]{width:100px;min-width:100px;}#Hxctimecard [title~=Task]{width:50px;min-width:50px;}#Hxctimecard [title~=Type]{width:100px;min-width:100px;}#Hxctimecard [title~=Location]{width:50px;min-width:50px;}#Hxctimecard input[title~=\"Hrs\"]{width:30px;}#Hxctimecard td.x1x:nth-of-type(5){display:none;}#Hxctimecard tr td.x1t:nth-of-type(5){display:none;}#Hxctimecard tr td.x24:nth-of-type(2){display:none;}#Hxctimecard td.x1x:nth-of-type(11){display:none;}#Hxctimecard tr td.x1t:nth-of-type(11){display:none;}#Hxctimecard tr td.x24:nth-of-type(8){display:none;}#Hxctimecard textarea.x8{min-width:160px;}";
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = css;
+document.getElementsByTagName('head')[0].appendChild(style);
