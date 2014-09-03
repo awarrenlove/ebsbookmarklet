3774cb357497421a6c12519a679badfd10989ade
Seth Broweleit <hello[@]getsetbro[.]com>
2014-09-03 12:19:52 -0500

    expandable inputs

--- greasemonkey/ebsrestyle1.user.js
+++ greasemonkey/ebsrestyle1.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name EBSrestyle1
 // @namespace http://getsetbro.com/ebsbookmarklet/
-// @description Simmer the table borders, fit more table on the page. 
+// @description Simmer the table borders, fit more table on the page.
 // @include http://stlcebs.perficient.com:8030/OA_HTML/*.jsp?*=809&*
 // @version	1.1
 // @grant none
 // ==/UserScript==
-var css = "#Hxctimecard table{border-collapse:collapse;width:auto;border:0 none}#Hxctimecard [src$='t.gif']{display:none}#Hxctimecard td{vertical-align:top}#Hxctimecard input,#Hxctimecard textarea{border:1px solid #ddd !important;min-height:22px}#Hxctimecard input,#Hxctimecard textarea[cols='25']{background-image:none !important;width:7.5vw;display:block}#Hxctimecard select{border:1px solid #ddd !important;min-width:220px}#Hxctimecard .x1x,#Hxctimecard .x2a,#Hxctimecard .x24{font-weight:normal;background-color:#eee}#Hxctimecard input ~ a{text-decoration:none;display:block;text-align:right;padding:5px 0 0 0}#Hxctimecard #tipIcon img,#Hxctimecard a img{display:none}#Hxctimecard button,#Hxctimecard #tipIcon:after,#Hxctimecard input ~ a:after{background-color:#69c;color:#fff;padding:0 5px;border-radius:8px}#Hxctimecard button{border:0 none;background-image:none !important;font-weight:normal}#Hxctimecard input ~ a:after{content:'Find';display:inline-block}#Hxctimecard #tipIcon:after{content:'Info';display:inline-block}#Hxctimecard a:hover:after,#Hxctimecard button:hover{background-color:#369}";
+var css = "#Hxctimecard table table table table select{min-width:220px}#Hxctimecard .x1x,#Hxctimecard .x2a,#Hxctimecard .x24{font-weight:normal;background-color:#eee}#Hxctimecard table table table table button,#Hxctimecard table table table tr:nth-child(6n) table a:after{border:0 none;background-image:none !important;font-weight:normal;background-color:#69c;color:#fff;border-radius:9px}#Hxctimecard table table table tr:nth-child(6n) table{border:0 none;border-collapse:collapse;width:100%}#Hxctimecard table table table tr:nth-child(6n) table td{border-color:#ddd;vertical-align:top}#Hxctimecard table table table tr:nth-child(6n) table input,#Hxctimecard table table table tr:nth-child(6n) table textarea{border:1px solid #ddd !important;width:100%;min-height:22px}#Hxctimecard table table table [src$='t.gif']{display:none}#Hxctimecard table table table tr:nth-child(6n) table a{display:block;text-align:right;text-decoration:none}#Hxctimecard table table table tr:nth-child(6n) table a img{display:none}#Hxctimecard table table table tr:nth-child(6n) table a:after{content:'Find';padding:0 5px 1px}#Hxctimecard table table table tr:nth-child(6n) table input:focus,#Hxctimecard table table table tr:nth-child(6n) table textarea:focus{width:255px}";
 var style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML = css;
